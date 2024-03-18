cc0ed2a4755182f47962fa54e0570baef0c8dbf4
Zhenyuan Zhang <zhang.zhenyuan@northeastern.edu>
2024-03-18 12:22:33 -0400

    updated server.js

--- server.js
+++ server.js
@@ -2,70 +2,59 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Models
+const BookInstance = require('./models/BookInstance'); 
+const Author = require('./models/Author'); 
+
 const port = 8000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-let Home = require('./pages/home');
-let Books = require('./pages/books');
-let BooksStatus = require('./pages/books_status');
-let Authors = require('./pages/authors');
-let BookDetails = require('./pages/book_details');
-let CreateBook = require('./pages/create_book');
-
-
-const mongoose = require('mongoose');
-const mongoDB = "mongodb://127.0.0.1:27017/my_library_db";
-// mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect(mongoDB);
+// MongoDB Connection
+const mongoDB = 'mongodb://127.0.0.1:27017/my_library_db';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.on('connected', function() {
+db.once('connected', () => {
   console.log('Connected to database');
 });
+
+// Middleware
 app.use(cors());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+// Route to display home page
 app.get('/home', (_, res) => {
-  Home.show_home(res);
-})
+  // Home logic here...
+});
 
+// Route to get available books
 app.get('/available', (_, res) => {
-  BooksStatus.show_all_books_status(res);
-})
+  
+  BookInstance.find({ status: 'available' })
+    .populate('book', 'title') 
+    .then(bookInstances => {
+      const availableBooks = bookInstances.map(bi => ({ title: bi.book.title, status: bi.status }));
+      res.json(availableBooks);
+    })
+    .catch(err => res.status(500).json({ error: err.message }));
+});
 
-app.get('/books', (_, res) => {
-  Books.show_books()
-    .then((data) => res.send(data))
-    .catch((_) => res.send('No books found'));
-})
-
+// Route to get all authors with their names and lifespans
 app.get('/authors', (_, res) => {
-  Authors.show_all_authors(res);
-})
+  
+  Author.find({})
+    .then(authors => {
+      const authorDetails = authors.map(author => ({ name: author.name, lifespan: author.lifespan }));
+      res.json(authorDetails);
+    })
+    .catch(err => res.status(500).json({ error: err.message }));
+});
 
-app.get('/book_dtls', (req, res) => {
-  BookDetails.show_book_dtls(res, req.query.id);
-})
 
-app.post('/newbook', (req, res) => {
-    const familyName = req.body.familyName;
-    const firstName = req.body.firstName;
-    const genreName = req.body.genreName;
-    const bookTitle = req.body.bookTitle;
-    if(familyName && firstName && genreName && bookTitle) {
-        CreateBook.new_book(res, familyName, firstName, genreName, bookTitle).catch(err => {
-                res.send('Failed to create new book ' + err);
-              });
-    }
-    else {
-        res.send('Invalid Inputs');
-    }
 
-})
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
